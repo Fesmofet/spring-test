9eeb65ae2a7a1fd9c4780a346688fcdbd2617944
Boris Andrzhiievskyi <borees2@gmail.com>
2020-10-07 19:25:57 +0300

    lesson 4

---
+++ src/main/java/org/springlesson1/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package org.springlesson1;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hungarian rhappsody";
+    }
+}
---
+++ src/main/java/org/springlesson1/Music.java
@@ -0,0 +1,5 @@
+package org.springlesson1;
+
+public interface Music {
+    String getSong();
+}
