67ac69715441dcce9f22c437df601d807bcbe47e
Boris Andrzhiievskyi <borees2@gmail.com>
2020-10-13 19:07:43 +0300

    lesson 6

--- src/main/java/org/springlesson1/ClassicalMusic.java
+++ src/main/java/org/springlesson1/ClassicalMusic.java
@@ -3,6 +3,6 @@ package org.springlesson1;
 public class ClassicalMusic implements Music {
     @Override
     public String getSong() {
-        return "Hungarian rhappsody";
+        return "Hungarian rhapsody";
     }
 }
